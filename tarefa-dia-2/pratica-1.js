4b7156daafb92ab867d1ed477a9521bd4c05ed46
laricissa <lari.cissa@hotmail.com>
2022-08-26 17:13:24 -0300

    pratica1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
-let nome = "larissa"
-let email = "lari.cissa@hotmail.com"
-let profissao = "administradora"
-let idade = "26"
-let viagemdossonhos = "Peru" 
+let nome = "(larissa)"
+let email = "(lari.cissa@hotmail.com)"
+let profissao = "(administradora)"
+let idade = "(26)"
+let lugar = "(Peru)"
+string = nome
+string = email
+string = profissao
+string = idade
+string = lugar
